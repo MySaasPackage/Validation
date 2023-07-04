ad2db11d65353dc9cbc853540bcf463a7648258a
Alef Castelo <alefaraujocastelo@gmail.com>
2023-07-04 16:03:39 -0600

    adding more tests to the ruleChain

--- tests/RuleChainTest.php
+++ tests/RuleChainTest.php
@@ -10,10 +10,17 @@ final class RuleChainTest extends TestCase
 {
     public function testRuleChainSuccessfully(): void
     {
-        $rule = RuleChain::create()
-            ->string()
-            ->email();
+        $email = RuleChain::create()->string()->email();
+        $this->assertTrue($email->validate('alef@gmail.com')->isSucceeded());
+        $this->assertFalse($email->validate('alef')->isSucceeded());
 
-        $this->assertTrue($rule->validate('alef@gmail.com')->isSucceeded());
+        $phone = RuleChain::create()->string()->phone();
+        $this->assertTrue($phone->validate('+525599999999')->isSucceeded());
+        $this->assertFalse($phone->validate('5599999999')->isSucceeded());
+
+        $password = RuleChain::create()->string()->length(8, 16);
+        $this->assertTrue($password->validate('myscretpassword')->isSucceeded());
+        $this->assertFalse($password->validate('week')->isSucceeded());
+        $this->assertFalse($password->validate('superlargpassword')->isSucceeded());
     }
 }
